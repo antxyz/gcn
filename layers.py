f5334aa5876f7755eeadee10c778a6b271f44892
wsh <979154554@qq.com>
2019-10-18 09:03:12 +0800

    su

--- layers.py
+++ layers.py
@@ -9,13 +9,14 @@ class MLPBlock(layers.Layer):
         self.heads=opt['heads']
         # 密集连接层的层数
         self.layers_dense=opt['layers_dense']
-        self.linear_1 = MutiHeadsAttn()
+        self.dim_sum=opt['dim_hidden']*self.heads
+        self.linear_1 = MutiHeadsAttn(self.heads, d_model, dropout=0.1)
         self.linear_2 = DenseConLayer()
-        self.linear_3 = Linear()
+        self.linear_3 = layers.Dense(self.dim_sum, activation='relu')
 
     def call(self,inputs):
         
-        output = self.linear_1(inputs)
+        output = self.linear_1(inputs, inputs, mask=None)
         output = self.linear_2(output)
         output = self.linear_3(output)
 
@@ -37,23 +38,40 @@ class LinearCom(layers.Layer):
     def call(self,input):
 
         return output
+# 全局函数
+def attention(query, key, mask=None, dropout=None):
+    d_k = query.size(-1)
+    scores = torch.matmul(query, key.transpose(-2, -1)) / math.sqrt(d_k)
+    if mask is not None:
+        scores = scores.masked_fill(mask == 0, -1e9)
+
+    p_attn = F.softmax(scores, dim=-1)
+    if dropout is not None:
+        p_attn = dropout(p_attn)
+
+    return p_attn
 
 #多头注意力层
+# 将图转换为全连接邻接矩阵、
+# 根据一个矩阵生成多个矩阵
 class MutiHeadsAttn(layers.Layer):
-    def __init__(self):
-        super().__init__(self)
+    def __init__(self, h, d_model, dropout=0.1):
+        super(MultiHeadAttention, self).__init__()
+        assert d_model % h == 0
 
-    def build(self,input_shape):
-        self.w = self.add_weight(shape=(input_shape[-1], self.units),
-                                initializer='random_normal',
-                                trainable=True)
-        self.b = self.add_weight(shape=(self.units,),
-                                initializer='random_normal',
-                                trainable=True)
-                                
-    def call(self,inputs):
-
-        return tf.matmul(inputs, self.w) + self.b
+        self.d_k = d_model // h
+        self.h = h
+        self.linears = tf.clones(tf.nn.Linear(d_model, d_model), 2)
+        self.dropout = tf.nn.Dropout(p=dropout)
+                            
+    def call(self, query, key, mask=None):
+        if mask is not None:
+            mask = mask.unsqueeze(1)
+        nbatches = query.size(0)
+        query, key = [l(x).view(nbatches, -1, self.h, self.d_k).transpose(1, 2)
+                             for l, x in zip(self.linears, (query, key))]
+        attn = attention(query, key, mask=mask, dropout=self.dropout)
+        return attn
 
 class DenseConBlock(layers.Layer):
     def __init__(self,opt):
@@ -104,14 +122,14 @@ class DenseConLayer(layers.Layer):
 
 
 class GraphConvLayer(layers.Layer):
-    def __init__(self):
+    def __init__(self,opt):
         super().__init__()
-
+        self.dim_hidden=opt['hidden']
     def build(self, input_shape):
-        self.w = self.add_weight(shape=(input_shape[-1], self.units),
+        self.w = self.add_weight(shape=(input_shape[-1], self.dim_hidden),
                                 initializer='random_normal',
                                 trainable=True)
-        self.b = self.add_weight(shape=(self.units,),
+        self.b = self.add_weight(shape=(self.dim_hidden,),
                                 initializer='random_normal',
                                 trainable=True)
 
@@ -125,9 +143,9 @@ class GraphConvLayer(layers.Layer):
         # 将邻接矩阵做归一化处理
         adj/=sum_row
         # 对输入进行计算
-        W_X = tf.matmul(self.w,inputs)
-        A_W_X=tf.matmul(W_X,tf.transpose(adj))
-        output=tf.nn.relu.A_W_X+self.b
+        W_X = tf.matmul(inputs,self.w)
+        A_W_X=tf.matmul(tf.transpose(adj),W_X)
+        output=tf.nn.relu(A_W_X+self.b)
         return output
 
 
